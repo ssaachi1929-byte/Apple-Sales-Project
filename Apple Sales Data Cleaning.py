a611f7bb06eadcce2cf27325a6c161ac4b2684ee
Shriya Sree Aachi <ssaachi1929@gmail.com>
2026-07-05 11:39:48 -0500

    Update Apple Sales Data Cleaning.py

--- Apple Sales Data Cleaning.py
+++ Apple Sales Data Cleaning.py
@@ -13,8 +13,13 @@ print(file.head())
 
 Updated_File = pd.read_csv("Updated Apple Sales and Product File.csv", encoding = "latin-1")
 
+new_Updated_File = Updated_File.dropna()
+
+Updated_File_duplicates = new_Updated_File.drop_duplicates()
+
 Top_products = Updated_File.groupby("Product_Name")["quantity"].sum()
 
 Top_product = Top_products.sort_values(ascending=False)
 
+print(Updated_File_duplicates)
 print(Top_product)
